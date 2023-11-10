46274544d175a41ff671281457ceb8dd8c6da63c
James Alanis <fjam07@gmail.com>
2023-11-09 18:35:29 -0600

    Initial dev commit

---
+++ ConfigurationHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+
+namespace ECommPostgresDemo;
+
+public class ConfigurationHelper
+{
+    private static readonly IConfigurationBuilder _configBuilder;
+    public static IConfiguration Config => _configBuilder.Build();
+
+    static ConfigurationHelper()
+    {
+        _configBuilder = new ConfigurationBuilder();
+        _configBuilder.AddUserSecrets(Assembly.GetExecutingAssembly(), optional: false);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using ECommPostgresDemo;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+IConfiguration config = ConfigurationHelper.Config;
+
+string connectionString = config.GetConnectionString("PostgreSQL");
+
+await using var dataSource = NpgsqlDataSource.Create(connectionString);
+
+// Retrieve all rows
+await using (var cmd = dataSource.CreateCommand("SELECT * FROM products"))
+await using (var reader = await cmd.ExecuteReaderAsync())
+{    
+    var products = new ArrayList();
+    while (await reader.ReadAsync())
+    {
+        var product = new 
+        { 
+            Id = reader.GetInt32(0), 
+            Name = reader.GetString(1), 
+            Type = reader.GetString(2)
+        };
+        products.Add(product);
+    }
+}
